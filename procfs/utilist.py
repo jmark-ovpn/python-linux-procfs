77054a7e8bd3991b8f51fc7b05896f3d412db819
John Kacur <jkacur@redhat.com>
2020-04-14 15:06:54 -0400

    python-linux-procfs: utilist: Clean-ups
    - Specify python3 explictly
    - Fixing tabbing and spacing
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- procfs/utilist.py
+++ procfs/utilist.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 # -*- python -*-
 # -*- coding: utf-8 -*-
 #
@@ -22,32 +22,32 @@ from six.moves import range
 
 
 def hexbitmask(l, nr_entries):
-	hexbitmask = []
-	bit = 0
-	mask = 0
-	for entry in range(nr_entries):
-		if entry in l:
-			mask |= (1 << bit)
-		bit += 1
-		if bit == 32:
-			bit = 0
-			hexbitmask.insert(0, mask)
-			mask = 0
+    hexbitmask = []
+    bit = 0
+    mask = 0
+    for entry in range(nr_entries):
+        if entry in l:
+            mask |= (1 << bit)
+        bit += 1
+        if bit == 32:
+            bit = 0
+            hexbitmask.insert(0, mask)
+            mask = 0
 
-	if bit < 32 and mask != 0:
-		hexbitmask.insert(0, mask)
+    if bit < 32 and mask != 0:
+        hexbitmask.insert(0, mask)
 
-	return hexbitmask
+    return hexbitmask
 
 def bitmasklist(line, nr_entries):
-	hexmask = line.strip().replace(",", "")
-	bitmasklist = []
-	entry = 0
-	bitmask = bin(int(hexmask, 16))[2::]
-	for i in reversed(bitmask):
-		if int(i) & 1:
-			bitmasklist.append(entry)
-		entry +=1
-		if entry == nr_entries:
-			break
-	return bitmasklist
+    hexmask = line.strip().replace(",", "")
+    bitmasklist = []
+    entry = 0
+    bitmask = bin(int(hexmask, 16))[2::]
+    for i in reversed(bitmask):
+        if int(i) & 1:
+            bitmasklist.append(entry)
+        entry += 1
+        if entry == nr_entries:
+            break
+    return bitmasklist
