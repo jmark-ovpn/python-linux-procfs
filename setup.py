cba70a4e239ae0bf626e6ccea7ee96d6f48890ff
John Kacur <jkacur@redhat.com>
2020-04-14 11:55:07 -0400

    python-linux-procfs: setup.py: Fix a few pythonesque issues
    - Remove tabs for spaces
    - Don't import unused glob module
    - Changes order of imports so that os comes before from os.path
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+import os
+from os.path import isfile, join
 from distutils.sysconfig import get_python_lib
 from setuptools import setup
-from os.path import isfile, join
-import glob
-import os
 
 if isfile("MANIFEST"):
-	os.unlink("MANIFEST")
+    os.unlink("MANIFEST")
 
 # Get PYTHONLIB with no prefix so --prefix installs work.
 PYTHONLIB = join(get_python_lib(standard_lib=1, prefix=''), 'site-packages')
 
 setup(name="python-linux-procfs",
-      version = "0.6.1",
-      description = "Linux /proc abstraction classes",
-      author = "Arnaldo Carvalho de Melo",
-      author_email = "acme@redhat.com",
-      url = "http://userweb.kernel.org/python-linux-procfs",
-      license = "GPLv2",
-      long_description =
+    version = "0.6.1",
+    description = "Linux /proc abstraction classes",
+    author = "Arnaldo Carvalho de Melo",
+    author_email = "acme@redhat.com",
+    url = "http://userweb.kernel.org/python-linux-procfs",
+    license = "GPLv2",
+    long_description =
 """\
 Abstractions to extract information from the Linux kernel /proc files.
 """,
-      packages = ["procfs"],
-      scripts = ['pflags'],
-      install_requires = ['six'],
-      )
+    packages = ["procfs"],
+    scripts = ['pflags'],
+    install_requires = ['six'],
+)
