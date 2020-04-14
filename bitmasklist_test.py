bc39214270bf80ecbe9ce3ec9e200b89df702ce8
John Kacur <jkacur@redhat.com>
2020-04-14 11:24:59 -0400

    python-linux-procfs: bitmasklist_test.py: python3 changes
    Only python3 is supported now
    
    Changes to python3
    - Specify python3 on the shebang
    - Get rid of future
    - Apply optional ws_comma from 2to3
    
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- bitmasklist_test.py
+++ bitmasklist_test.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-from __future__ import print_function
+#!/usr/bin/python3
+
 from procfs import bitmasklist
 
 class bitmasklist_test:
@@ -52,17 +52,17 @@ t.bitmasklist_test()
 
 # CPU 0, 32
 t = \
-    bitmasklist_test("00000000,00000000,00000000,00000000,00000001,00000001", 44, [0,32])
+    bitmasklist_test("00000000,00000000,00000000,00000000,00000001,00000001", 44, [0, 32])
 t.bitmasklist_test()
 
 # cpu 0-15
 t = \
-    bitmasklist_test("ffff", 44, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) 
+    bitmasklist_test("ffff", 44, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]) 
 t.bitmasklist_test()
 
 #cpu 0-71
 t = \
-    bitmasklist_test("ff,ffffffff,ffffffff", 96, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71])
+    bitmasklist_test("ff,ffffffff,ffffffff", 96, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71])
 t.bitmasklist_test()
 
 exit(bitmasklist_test.unit_test_result)
